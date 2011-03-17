2fec03360d6e3f9127a2b3ab59ec52f0d0f63eab
jgetto <jacob.getto@students.olin.edu>
2011-03-17 15:36:43 -0400

    cleaned up

--- src/edu/olin/snapdragon/snapdragon.java
+++ src/edu/olin/snapdragon/snapdragon.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.Size;
@@ -15,7 +16,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class snapdragon extends Activity {
+public class Snapdragon extends Activity implements SurfaceHolder.Callback, OnClickListener {
 	
 	private SurfaceView preview=null;
 	private SurfaceHolder previewHolder=null;
@@ -28,24 +29,23 @@ public class snapdragon extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        
-        preview=(SurfaceView)findViewById(R.id.preview);
-		previewHolder=preview.getHolder();
-//		previewHolder.addCallback(surfaceCallback);
-//		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-       
-		camera=Camera.open();
-		
-		try {
-			camera.setPreviewDisplay(previewHolder);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		camera.startPreview();
 		
     }
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
